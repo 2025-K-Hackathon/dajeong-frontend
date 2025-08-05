748c994eb1fcbe52db079a830ec04dc2a374e010
nakyeong <yunna3425@naver.com>
2025-08-06 06:21:11 +0900

    Feat: 회화 카드 디테일 페이지 UI 구현

---
+++ src/constant/cardContentData.js
@@ -0,0 +1,42 @@
+export const CardContent = [
+    {
+        "id": 52,
+        "situation": "HOSPITAL",
+        "inputText": "نحتاج إلى مساعدتك!",
+        "translatedText": "여러분의 도움이 필요합니다!",
+        "iconUrl": "/images/icons/hospital.png",
+        "ttsUrl": "/api/tts/synthesize/52"
+    },
+    {
+        "id": 102,
+        "situation": "HOSPITAL",
+        "inputText": "Can I schedule an appointment?",
+        "translatedText": "진료 예약할 수 있을까요?",
+        "iconUrl": "/images/icons/hospital.png",
+        "ttsUrl": "/api/tts/synthesize/102"
+    },
+    {
+        "id": 103,
+        "situation": "HOSPITAL",
+        "inputText": "Can I schedule an appointment?",
+        "translatedText": "진료 예약할 수 있을까요?",
+        "iconUrl": "/images/icons/hospital.png",
+        "ttsUrl": "/api/tts/synthesize/102"
+    },
+    {
+        "id": 104,
+        "situation": "HOSPITAL",
+        "inputText": "Can I schedule an appointment?",
+        "translatedText": "진료 예약할 수 있을까요?",
+        "iconUrl": "/images/icons/hospital.png",
+        "ttsUrl": "/api/tts/synthesize/102"
+    },
+    {
+        "id": 105,
+        "situation": "HOSPITAL",
+        "inputText": "Can I schedule an appointment?",
+        "translatedText": "진료 예약할 수 있을까요?",
+        "iconUrl": "/images/icons/hospital.png",
+        "ttsUrl": "/api/tts/synthesize/102"
+    },
+]
--- src/screens/CardDetail.js
+++ src/screens/CardDetail.js
@@ -1,12 +1,166 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
+import styled from 'styled-components';
+import { CustomHeader } from './../components';
+import { Colors } from '../theme';
+import Hospital from '../../assets/images/home/hospital.png';
+import { CardContent } from './../constant/cardContentData';
+import Plus from '../../assets/images/livingInfo/plus.png';
+import Sound from '../../assets/images/livingInfo/sound.png';
 
 const CardDetail = () => {
     return (
-        <View>
-            <Text>회화 카드 디테일</Text>
-        </View>
+        <Layout>
+            <CustomHeader type="text" text="회화 카드" />
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+            >
+                <Wrapper>
+                    <CardWrapper>
+                        <Icon source={Hospital} />
+                        <TextWrapper>
+                            <Title>병원 접수</Title>
+                            <Foreign>Hospital Registration</Foreign>
+                        </TextWrapper>
+                        {CardContent.map((card, index) => (
+                            <ConversationWrapper
+                                key={index}
+                                last={index === CardContent.length - 1}
+                            >
+                                <ConversationTextWrapper>
+                                    <Korean>{card.translatedText}</Korean>
+                                    <Foreign>{card.inputText}</Foreign>
+                                </ConversationTextWrapper>
+                                <SoundWrapper>
+                                    <Image source={Sound}/>
+                                </SoundWrapper>
+                            </ConversationWrapper>
+                        ))}
+                    </CardWrapper>
+                    <PlusWrapper>
+                        <Image source={Plus} />
+                    </PlusWrapper>
+                </Wrapper>
+            </ScrollView>
+        </Layout>
     )
 }
+
+const Layout = styled.View`
+    background-color: #FFFFFF;
+    flex: 1;
+`
+
+const Wrapper = styled.View`
+    padding: 0 15px;
+`
+
+const CardWrapper = styled.View`
+    border: 1px solid rgba(255, 126, 148, 0.5);
+    width: 100%;
+    border-radius: 15px;
+    background-color: #FFFFFF;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+    padding: 20px 10px 18px 10px;
+    /* 그림자 */
+    shadow-color: rgba(0, 0, 0, 1);
+    shadow-offset: {
+        width: 0px;
+        height: 0px;
+    };
+    shadow-opacity: 0.25;
+    shadowRadius: 4px;
+    elevation: 2;
+`
+
+const Icon = styled(Image)`
+    width: 120px;
+    height: 120px;
+`
+
+const TextWrapper = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 0 5px 5px 5px;
+    border-color: #E6E6E6;
+    border-bottom-width: 1;
+    margin-bottom: 20px;
+`
+
+const Title = styled.Text`
+    font-size: 20px;
+    line-height: 28px;
+    font-family: 'bold';
+    margin-top: 8px;
+`
+
+const Foreign = styled.Text`
+    font-size: 15px;
+    line-height: 22px;
+    font-family: 'regular';
+    color: #7F7F7F;
+`
+
+const ConversationWrapper = styled.View`
+    padding-bottom: 5px;
+    width: 100%;
+    border-color: #E6E6E6;
+    border-bottom-width: ${({last}) => last ? '0px' : '1px'};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+`
+
+const ConversationTextWrapper = styled.View`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`
+
+const Korean = styled.Text`
+    font-size: 17px;
+    font-family: 'bold';
+    line-height: 22px;
+    margin-bottom: 3px;
+    margin-top: 10px;
+`
+
+const SoundWrapper = styled(TouchableOpacity)`
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    border: 1px solid #8B8B8B;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #FFFFFF;
+    margin-left: 10px;
+    /* 그림자 */
+    shadow-color: rgba(0, 0, 0, 1);
+    shadow-offset: {
+        width: 0px;
+        height: 0px;
+    };
+    shadow-opacity: 0.25;
+    shadowRadius: 4px;
+    elevation: 2;
+`
+
+const PlusWrapper = styled(TouchableOpacity)`
+    background-color: #E5E5E5;
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 18px auto 10px auto;
+`
 
 export default CardDetail
