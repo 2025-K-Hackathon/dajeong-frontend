9eab1d9ac342780f94cc4b2ce4ce491b4e721488
nakyeong <yunna3425@naver.com>
2025-08-10 05:42:33 +0900

    Feat: 마이페이지 API 연결

--- src/screens/Mypage.js
+++ src/screens/Mypage.js
@@ -1,19 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { styled } from 'styled-components';
 import { CustomMiniHeader } from '../components';
 import { UserData } from './../constant/userData';
 import { Colors } from './../theme';
+import axiosInstance from './../utils/axiosInstance';
 
 const Mypage = () => {
+    const [name, setName] = useState('');
+    const [id, setId] = useState();
+    const [nationality, setNationality] = useState();
+    const [age, setAge] = useState();
+    const [region, setRegion] = useState();
+    const [isMarried, setIsMarried] = useState();
+    const [hasChildren, setHasChildren] = useState();
+    
+    const countries = [
+        { name: '베트남', english: 'Vietnam', value: 'VIETNAM' },
+        { name: '중국', english: 'China', value: 'CHINA' },
+        { name: '필리핀', english: 'Philippines', value: 'PHILIPPINES' },
+        { name: '태국', english: 'Thailand', value: 'THAILAND' },
+        { name: '인도네시아', english: 'Indonesia', value: 'INDONESIA' },
+        { name: '기타', value: 'ETC' },
+        { name: '비공개', value: 'PRIVATE' },
+    ];
+
+    const regions = [
+        { name: '수도권', value: 'CAPITAL' },
+        { name: '충청도', value: 'CHUNGCHEONG' },
+        { name: '강원도', value: 'GANGWON' },
+        { name: '경상도', value: 'GYEONGSANG' },
+        { name: '전라도', value: 'JEOLLA' },
+        { name: '제주도', value: 'JEJU' },
+        { name: '비공개', value: 'PRIVATE' },
+    ];
+
+    const children = [
+        { name: 'o', value: 'YES' },
+        { name: 'x', value: 'NO' },
+        { name: '답변 안함', value: 'PRIVATE' },
+    ];
+
+    const getName = (data, array) => {
+        const value = array.find(item => item.value === data);
+        return value ? value.name : value;
+    }
+
+    const handleInformation = async () => {
+        try {
+            const response = await axiosInstance.get('/api/users/me');
+            console.log('내 정보 조회', response.data);
+            setName(response.data.name);
+            setId(response.data.username);
+            setNationality(getName(response.data.nationality, countries));
+            setAge(Math.floor((2025 - response.data.age) / 10));
+            setRegion(getName(response.data.region, regions));
+            setIsMarried(response.data.married);
+            setHasChildren(getName(response.data.hasChildren, children));
+        } catch(error) {
+            console.log('내 정보 조회 실패', error.response);
+        }
+    }
+
+    useEffect(() => {
+        handleInformation();
+    }, [])
+
     return (
         <Background>
             <Wrapper>
                 <CustomMiniHeader text="마이페이지" marginBottom={39} />
-                <Title>{UserData.name}</Title>
+                <Title>{name}</Title>
                 <CategoryWrapper>
                     <IdCategory>아이디</IdCategory>
-                    <Id>{UserData.username}</Id>
+                    <Id>{id}</Id>
                 </CategoryWrapper>
             </Wrapper>
             <Line />
@@ -21,23 +81,23 @@ const Mypage = () => {
                 <Title>회원 정보</Title>
                 <CategoryWrapper>
                     <Category>출신 국가</Category>
-                    <Value>{UserData.nationality}</Value>
+                    <Value>{nationality}</Value>
                 </CategoryWrapper>
                 <CategoryWrapper>
                     <Category>연령대</Category>
-                    <Value>{UserData.age}</Value>
+                    <Value>{age}0대</Value>
                 </CategoryWrapper>
                 <CategoryWrapper>
                     <Category>지역</Category>
-                    <Value>{UserData.region}</Value>
+                    <Value>{region}</Value>
                 </CategoryWrapper>
                 <CategoryWrapper>
                     <Category>결혼 여부</Category>
-                    <Value>{UserData.married ? '기혼'  : '미혼'}</Value>
+                    <Value>{isMarried ? '기혼'  : '미혼'}</Value>
                 </CategoryWrapper>
                 <CategoryWrapper>
                     <Category>자녀 유무</Category>
-                    <Value>{UserData.hasChildren ? 'o' : 'x'}</Value>
+                    <Value>{hasChildren}</Value>
                 </CategoryWrapper>
             </InfoWrapper>
             <Line />
